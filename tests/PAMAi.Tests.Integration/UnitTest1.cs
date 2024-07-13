0471d2918f7a704c0bd0bd88bb0f75880310acf7
Emmanuel Allison <emmallison13@gmail.com>
2024-07-13 13:08:03 +0100

    feat: add integration test project

---
+++ tests/PAMAi.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PAMAi.Tests.Integration;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
