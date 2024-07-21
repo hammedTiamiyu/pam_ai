8256476f184ce6220a1574f5b8d749a54b2dd765
Babatunde Ogundele <stephen@kreatesell.com>
2024-07-22 00:50:47 +0100

    notification service revamp for testing SMS Notifs

--- src/PAMAi.API/Controllers/NotificationsController.cs
+++ src/PAMAi.API/Controllers/NotificationsController.cs
@@ -8,11 +8,13 @@ namespace PAMAi.API.Controllers;
 /// <summary>
 /// Notifications for SMS and email
 /// </summary>
-public class NotificationsController : ControllerBase
+public class NotificationsController : BaseController
 {
     private readonly NotificationService _notificationService;
+    private readonly IActionResult _genericFailedLoginResponse;
 
-    public NotificationsController(NotificationService notificationService)
+    public NotificationsController(NotificationService notificationService, 
+        IHttpContextAccessor httpContextAccessor) : base (httpContextAccessor)
     {
         _notificationService = notificationService;
     }
@@ -24,6 +26,8 @@ public class NotificationsController : ControllerBase
     /// <param name="request"></param>
     /// <returns></returns>
     [HttpPost("password-reset")]
+    [ProducesResponseType(typeof(SmsResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public async Task<IActionResult> SendPasswordResetSms([FromBody] PasswordResetRequest request)
     {
         var result = await _notificationService.SendCredentialsAsync(request.PhoneNumber, request.ResetLink);
@@ -36,9 +40,14 @@ public class NotificationsController : ControllerBase
     /// <param name="request"></param>
     /// <returns></returns>
     [HttpPost("test-sms")]
+    [ProducesResponseType(typeof(SmsResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public async Task<IActionResult> TestSmsNotification([FromBody] SmsRequest request)
     {
         var result = await _notificationService.TestSMSAsync(request.To, request.Sms);
-        return result.IsSuccess ? Ok(result) : BadRequest(result.Error);
+        //return result.IsSuccess ? Ok(result) : BadRequest(result.Error);
+        return result.Match(
+            onSuccess: () => Ok(result),
+            onFailure: ErrorResult);
     }
 }
